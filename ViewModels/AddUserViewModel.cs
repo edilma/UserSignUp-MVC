da315dd0c1d85d1b6f3a5d706ed0fedf8a8ae518
Edilma <edilmaria@hotmail.com>
2019-11-22 12:07:33 -0500

    initial changes in model views studio

---
+++ ViewModels/AddUserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserSignup.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required (ErrorMessage = "Passwords do not match")]
+
+        [Display(Name = "Verify Password")]
+        [Compare("Password")]
+        public string Verify { get; set; }
+
+    }
+}
